fd4cf817452f242be6b8dd72f6d6a7bea747176a
Tetre101 <basilpeter989@gmail.com>
2022-10-04 01:36:06 +0200

    my employees database project

---
+++ client/src/Components/App.styled.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+
+export const AppBOX =styled.div`
+  display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color:#FF2400;
+`;
+  export const Appinformation = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  `;
+  
+  export const Appinput=styled.input`
+  width: 300px;
+  height: 50px;
+  font-size: 20px;
+  padding-left: 10px;
+  margin: 5px;
+  `;
+  export const Appbutton = styled.button`
+  width: 320px;
+  height: 50px;
+  margin-top: 15px;
+  `;
+  export const Appemployees= styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  ` ;
+  export const Appemployee = styled.div`
+  width: 700px;
+  height: auto;
+  border: 1px solid black;
+  margin: 10px;
+  display: flex;
+  padding: 10px;
+  font-family: 'Courier New', Courier, monospace;
+  background-color: royalblue;
+  `;
+  export const Apph3 = styled.h3`
+    margin: 20px;
+`;
+ 
---
+++ client/src/Components/Page.js
@@ -0,0 +1,141 @@
+import React from "react";
+import { useState } from "react";
+import {
+  AppBOX,
+  Appinformation,
+  Appemployee,
+  Appemployees,
+  Appinput,
+  Appbutton,
+  Apph3,
+} from "./App.styled";
+import axios from "axios";
+
+export const Page = () => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+const [employeelist, setEmployeeList] = useState([])
+const [newPosition, setNewPosition] = useState("")
+  
+
+
+const AddEmployees = () => {
+    axios
+      .post("http://localhost:9090/create", {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        wage: wage,
+      })
+      .then((response) => {
+        setEmployeeList([...employeelist,{ name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,}]);
+      });
+  };
+
+const GetEmployees =()=>{
+  axios
+  .get("http://localhost:9090/employees", {
+  })
+  .then((response) => {
+    setEmployeeList(response.data);
+  });
+}
+
+const updateEmployeePosition =(id)=>{
+  axios.put("http://localhost:9090/update",{
+position: newPosition, id: id, 
+  })
+  .then((response)=>{
+   console.log("UPDATED")
+  })
+}
+
+const deleteEmployee =(id)=>{
+  axios.delete(`http://localhost:9090/delete/${id}`)
+  .then((response)=>{
+    setEmployeeList(employeelist.filter((e)=>{
+      return e.id !== id  
+    }))
+  })
+}
+  return (
+    <AppBOX>
+      <Appinformation>
+        <label>NAME</label>
+        <Appinput
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label>AGE</label>
+        <Appinput
+          type="number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <label>COUNTRY</label>
+        <Appinput
+          type="text"
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        />
+        <label>POSITION</label>
+        <Appinput
+          type="text"
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
+        <label>WAGE</label>
+        <Appinput
+          type="number"
+          onChange={(e) => {
+            setWage(e.target.value);
+          }}
+        />
+        <Appbutton onClick={AddEmployees}>ADD EMPLOYEES</Appbutton>
+      </Appinformation>
+      <Appemployees>
+        <Appbutton onClick={GetEmployees}>SHOW EMPLOYEES</Appbutton>
+        {employeelist.map((e)=>{
+          return(<Appemployee>
+            <div>
+              <Apph3>NAME: {e.name}</Apph3>
+              <Apph3>AGE: {e.age}</Apph3>
+              <Apph3>WAGE: {e.wage}</Apph3>
+              <Apph3>POSITION: {e.position}</Apph3>
+              <Apph3>COUNTRY: {e.country}</Apph3>
+<div>
+  <Appinput type='text' onChange={(e) => {
+            setNewPosition(e.target.value);
+          }}  />
+          <Appbutton onClick={()=>{
+            updateEmployeePosition(e.id)
+          }}>UPDATE POSITION</Appbutton>
+
+          <Appbutton onClick={()=>{
+            deleteEmployee(e.id)
+          }}>DELETE</Appbutton>
+</div>
+
+
+
+
+            </div>
+          </Appemployee>)
+        })}
+      </Appemployees>
+    </AppBOX>
+  );
+};

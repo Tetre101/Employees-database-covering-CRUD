859026494c78be6c128b99a9ba90f9eda3d6bd96
Tetre101 <basilpeter989@gmail.com>
2023-05-13 23:30:08 +0200

    proxy setup for launch

--- client/src/Components/Page.js
+++ client/src/Components/Page.js
@@ -9,7 +9,7 @@ import {
   Appbutton,
   Apph3,
 } from "./App.styled";
-import axios from "axios";
+import axios from "../api/Index";
 
 export const Page = () => {
   const [name, setName] = useState("");
@@ -17,14 +17,12 @@ export const Page = () => {
   const [country, setCountry] = useState("");
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
-const [employeelist, setEmployeeList] = useState([])
-const [newPosition, setNewPosition] = useState("")
-  
+  const [employeelist, setEmployeeList] = useState([]);
+  const [newPosition, setNewPosition] = useState("");
 
-
-const AddEmployees = () => {
+  const AddEmployees = () => {
     axios
-      .post("http://localhost:9090/create", {
+      .post("/create", {
         name: name,
         age: age,
         country: country,
@@ -32,40 +30,45 @@ const AddEmployees = () => {
         wage: wage,
       })
       .then((response) => {
-        setEmployeeList([...employeelist,{ name: name,
-          age: age,
-          country: country,
-          position: position,
-          wage: wage,}]);
+        setEmployeeList([
+          ...employeelist,
+          {
+            name: name,
+            age: age,
+            country: country,
+            position: position,
+            wage: wage,
+          },
+        ]);
       });
   };
 
-const GetEmployees =()=>{
-  axios
-  .get("http://localhost:9090/employees", {
-  })
-  .then((response) => {
-    setEmployeeList(response.data);
-  });
-}
+  const GetEmployees = () => {
+    axios.get("/employees", {}).then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
 
-const updateEmployeePosition =(id)=>{
-  axios.put("http://localhost:9090/update",{
-position: newPosition, id: id, 
-  })
-  .then((response)=>{
-   console.log("UPDATED")
-  })
-}
+  const updateEmployeePosition = (id) => {
+    axios
+      .put("/update", {
+        position: newPosition,
+        id: id,
+      })
+      .then((response) => {
+        console.log("UPDATED");
+      });
+  };
 
-const deleteEmployee =(id)=>{
-  axios.delete(`http://localhost:9090/delete/${id}`)
-  .then((response)=>{
-    setEmployeeList(employeelist.filter((e)=>{
-      return e.id !== id  
-    }))
-  })
-}
+  const deleteEmployee = (id) => {
+    axios.delete(`/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeelist.filter((e) => {
+          return e.id !== id;
+        })
+      );
+    });
+  };
   return (
     <AppBOX>
       <Appinformation>
@@ -108,32 +111,41 @@ const deleteEmployee =(id)=>{
       </Appinformation>
       <Appemployees>
         <Appbutton onClick={GetEmployees}>SHOW EMPLOYEES</Appbutton>
-        {employeelist.map((e)=>{
-          return(<Appemployee>
-            <div>
-              <Apph3>NAME: {e.name}</Apph3>
-              <Apph3>AGE: {e.age}</Apph3>
-              <Apph3>WAGE: {e.wage}</Apph3>
-              <Apph3>POSITION: {e.position}</Apph3>
-              <Apph3>COUNTRY: {e.country}</Apph3>
-<div>
-  <Appinput type='text' onChange={(e) => {
-            setNewPosition(e.target.value);
-          }}  />
-          <Appbutton onClick={()=>{
-            updateEmployeePosition(e.id)
-          }}>UPDATE POSITION</Appbutton>
+        {employeelist.map((e) => {
+          return (
+            <Appemployee>
+              <div>
+                <Apph3>NAME: {e.name}</Apph3>
+                <Apph3>AGE: {e.age}</Apph3>
+                <Apph3>WAGE: {e.wage}</Apph3>
+                <Apph3>POSITION: {e.position}</Apph3>
+                <Apph3>COUNTRY: {e.country}</Apph3>
+                <div>
+                  <Appinput
+                    type="text"
+                    onChange={(e) => {
+                      setNewPosition(e.target.value);
+                    }}
+                  />
+                  <Appbutton
+                    onClick={() => {
+                      updateEmployeePosition(e.id);
+                    }}
+                  >
+                    UPDATE POSITION
+                  </Appbutton>
 
-          <Appbutton onClick={()=>{
-            deleteEmployee(e.id)
-          }}>DELETE</Appbutton>
-</div>
-
-
-
-
-            </div>
-          </Appemployee>)
+                  <Appbutton
+                    onClick={() => {
+                      deleteEmployee(e.id);
+                    }}
+                  >
+                    DELETE
+                  </Appbutton>
+                </div>
+              </div>
+            </Appemployee>
+          );
         })}
       </Appemployees>
     </AppBOX>
---
+++ client/src/api/Index.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:9090";
+// const BASE_URL = 'http://localhost:3000/api/v1';
+
+export default axios.create({
+  baseURL: BASE_URL,
+});
